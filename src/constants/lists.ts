7a6df5e41bdc8c763653cccc30497030d1b1309e
appleswapdev <info@appleswap.club>
2022-02-17 15:43:35 +0800

    Update lists.ts

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,6 +1,6 @@
 // the Appleswap Default token list lives here
 
-export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/appleswapdev/token-list/main/appleswap.tokenlist.json'
+export const DEFAULT_TOKEN_LIST_URL = 'https://app.appleswap.club/appleswap.tokenlist.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL
